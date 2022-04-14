f409339c01daea5014a87411f02478082e1dc286
YU-GREEE-TEA <90495076+YU-GREEE-TEA@users.noreply.github.com>
2022-04-15 00:41:41 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -4,10 +4,10 @@ main () {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) { //0,1,2...,n-1
         for (int j = 0; j < n-i; j++) {
-            if (i % 2) {
-        	printf("+");   		
+            if (i % 2 == 0) {
+            	printf("*");   		
             } else {
-        	printf("*");
+            	printf("+");
             }
         }
         printf("\n");
